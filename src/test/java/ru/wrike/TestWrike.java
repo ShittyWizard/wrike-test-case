1620b97c73dd0bf50a4ecf3e6d272fae19710245
ShittyWizard <anisimov.develop@gmail.com>
2019-04-14 02:12:09 +0300

    Change sout() to log.info()

--- src/test/java/ru/wrike/TestWrike.java
+++ src/test/java/ru/wrike/TestWrike.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.Random;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
@@ -20,30 +19,40 @@ public class TestWrike {
 
     @BeforeClass
     public static void startWebDriver() {
+        logger.info("Test started");
         String url = "https://www.wrike.com/";
         webDriver.get(url);
-        logger.log(Level.INFO, "Web driver for url: " + url + " starts...");
+        logger.info("Web driver for url: " + url + " starts...");
 
         webDriver.manage().window().maximize();
-        logger.log(Level.INFO, "Screen of web driver maximised...");
+        logger.info("Screen of web driver maximised...");
     }
 
     @Test
     public void testGetStartedCase() {
 //        TODO: change all System.out.println() to logger.log()
         assert webDriver.getTitle().equals("Your online project management software - Wrike");
-        logger.log(Level.INFO, "Test started");
-        System.out.println("Try to click on 'Get started for free' button...");
-        WebElement buttonGetStarted = webDriver.findElement(By.xpath("//div[@class='wg-header__desktop']" +
-                "//button[@class='wg-header__free-trial-button wg-btn wg-btn--green']"));
 
-        assert buttonGetStarted.isDisplayed();
-        assert buttonGetStarted.isEnabled();
+        if (webDriver.findElements(By.xpath("//form[@class='wg-header__free-trial-form wg-form bf_form_init']" +
+                "//button[text()='Get started']")).size() != 0) {
+            logger.info("'Get started for free' button is on the page");
+            assert true;
+            logger.info("Try to click on 'Get started for free' button...");
+//            WebElement buttonGetStarted = webDriver.findElement(By.xpath("//div[@class='wg-header__desktop']" +
+//                    "//button[@class='wg-header__free-trial-button wg-btn wg-btn--green']"));
 
-        buttonGetStarted.click();
-        System.out.println("Successful click on 'Get started for free' button...\n");
+            WebElement buttonGetStarted = webDriver.findElement(By.xpath("//form[@class='wg-header__free-trial-form wg-form bf_form_init']" +
+                    "//button[text()='Get started']"));
 
-        System.out.println("Try to input random email address...");
+            buttonGetStarted.click();
+            logger.info("Successful click on 'Get started for free' button...");
+        } else {
+            logger.info("There is no 'Get started for free' button...");
+            assert false;
+        }
+
+
+        logger.info("Try to input random email address...");
         WebElement inputEmailAddress = webDriver.findElement(By.xpath("//input[@class='wg-input modal-form-trial__input']"));
 
         assert inputEmailAddress.isDisplayed();
@@ -51,9 +60,9 @@ public class TestWrike {
 
         String randomEmailAddress = generateRandomEmailAddress();
         inputEmailAddress.sendKeys(randomEmailAddress);
-        System.out.println("Successful input random email address...\n");
+        logger.info("Successful input random email address...");
 
-        System.out.println("Try to click on 'Create Wrike account' button...");
+        logger.info("Try to click on 'Create Wrike account' button...");
         WebElement buttonCreateAccount = webDriver.findElement(By.xpath("//button[@class='wg-btn wg-btn--blue modal-form-trial__submit']"));
 
         assert buttonCreateAccount.isDisplayed();
@@ -61,7 +70,7 @@ public class TestWrike {
 
         String currentUrl = webDriver.getCurrentUrl();
         buttonCreateAccount.click();
-        System.out.println("Successful click on 'Create Wrike account' button...\n");
+        logger.info("Successful click on 'Create Wrike account' button...");
 
         WebDriverWait webDriverWait = new WebDriverWait(webDriver, 10);
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@class='survey-form']")));
@@ -74,27 +83,27 @@ public class TestWrike {
         assert form.isEnabled();
 
         // Fill form for question "How would you describe your interest in using a solution like Wrike?"
-        System.out.println("Try to fill first part of form (Interest in solution)");
+        logger.info("Try to fill first part of form (Interest in solution)");
 
         WebElement interestInSolution = getRandomElementFromList(form.findElements(By.xpath("//div[@data-code='interest_in_solution']/label")));
         assert interestInSolution.isDisplayed();
         assert interestInSolution.isEnabled();
 
         interestInSolution.click();
-        System.out.println("Successful first part of form (Interest in solution) \n");
+        logger.info("Successful first part of form (Interest in solution)  ");
 
         // Fill form for question "Ideally, how many total team members will eventually be using Wrike?"
-        System.out.println("Try to fill second part of form (Team members)");
+        logger.info("Try to fill second part of form (Team members)");
 
         WebElement teamMembers = getRandomElementFromList(form.findElements(By.xpath("//div[@data-code='team_members']/label")));
         assert teamMembers.isDisplayed();
         assert teamMembers.isEnabled();
 
         teamMembers.click();
-        System.out.println("Successful second part of from (Team members)\n");
+        logger.info("Successful second part of from (Team members) ");
 
         // Fill form for question "Does your team follow a process for managing work?"
-        System.out.println("Try to fill third part of form (Primary Business)");
+        logger.info("Try to fill third part of form (Primary Business)");
 
         WebElement primaryBusiness = getRandomElementFromList(form.findElements(By.xpath("div[@data-code='primary_business']/label")));
         assert primaryBusiness.isDisplayed();
@@ -105,37 +114,38 @@ public class TestWrike {
 //        if (primaryBusiness.getText().equals("Other")) {
 //            primaryBusiness.findElement(By.xpath("//input")).sendKeys(new String[] {generateRandomString()});
 //        }
-        System.out.println("Successful third part of form (Primary Business)\n");
+        logger.info("Successful third part of form (Primary Business) ");
 
-        System.out.println("Try to click on 'Resend email' button");
+        logger.info("Try to click on 'Resend email' button");
 //        TODO: Optimize XPath request 1
         WebElement buttonResendEmail = webDriver.findElement(By.xpath("//div[@class='wg-grid']/div/p/button"));
         assert buttonResendEmail.isDisplayed();
         assert buttonResendEmail.isEnabled();
 
         buttonResendEmail.click();
-        System.out.println("Successful click on 'Resend email' button \n");
+        logger.info("Successful click on 'Resend email' button  ");
 
 //        Check that section "Follow us" at the site footer contains the "Twitter" button that leads to the correct url and has the correct icon;
-        System.out.println("Check Follow us section: Twitter");
+        logger.info("Check Follow us section: Twitter");
 //        TODO: Optimize XPath request 2
         WebElement followTwitterWrike = webDriver.findElement(By.xpath("//ul[@class='wg-footer__social-list']//a[@href='https://twitter.com/wrike']/.."));
         assert followTwitterWrike.isDisplayed();
         assert followTwitterWrike.isEnabled();
 
-        System.out.println("Successful check Twitter \n");
+        logger.info("Successful check Twitter  ");
 
 //        TODO: Check logo of Twitter
-        System.out.println("Check correct logo for Twitter");
-//        assert followTwitterWrike.findElement(By.xpath("//svg/title/..")).getAttribute("id").equals("twitter");
-        System.out.println("Successful check correct logo for Twitter\n");
+        logger.info("Check correct logo for Twitter");
+        assert followTwitterWrike.findElement(By.xpath("//*[local-name()='use' and @*|href='/content/themes/wrike/dist/img/sprite/vector/footer-icons.symbol.svg?v2#twitter']")).isDisplayed();
+//        assert followTwitterWrike.findElement(By.xpath("//*[contains(@*|href,'twitter')]")).isDisplayed();
+        logger.info("Successful check correct logo for Twitter ");
 //        TODO: Report by allure plugin
     }
 
     @AfterClass
     public static void quitWebDriver() {
         webDriver.quit();
-        System.out.println("Web driver quits");
+        logger.info("Web driver quits");
     }
 
     /**
