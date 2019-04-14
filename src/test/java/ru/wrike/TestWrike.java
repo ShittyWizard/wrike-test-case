82c57b48683b50abb55156fffbee00946d1875c4
ShittyWizard <anisimov.develop@gmail.com>
2019-04-14 03:35:33 +0300

    Refactor all assertions - create method isElementVisible() - optimized xpath queries

--- src/test/java/ru/wrike/TestWrike.java
+++ src/test/java/ru/wrike/TestWrike.java
@@ -30,45 +30,39 @@ public class TestWrike {
 
     @Test
     public void testGetStartedCase() {
-//        TODO: change all System.out.println() to logger.log()
-        assert webDriver.getTitle().equals("Your online project management software - Wrike");
+        // Check that 'Get Started for free' button is visible
+        By buttonGetStartedXpath = By.xpath("//div[@class='wg-header__desktop']" +
+                "//button[contains(text(), 'Get started')]");
+        assert isElementVisible(buttonGetStartedXpath);
 
-        if (webDriver.findElements(By.xpath("//form[@class='wg-header__free-trial-form wg-form bf_form_init']" +
-                "//button[text()='Get started']")).size() != 0) {
-            logger.info("'Get started for free' button is on the page");
-            assert true;
-            logger.info("Try to click on 'Get started for free' button...");
-//            WebElement buttonGetStarted = webDriver.findElement(By.xpath("//div[@class='wg-header__desktop']" +
-//                    "//button[@class='wg-header__free-trial-button wg-btn wg-btn--green']"));
+        WebElement buttonGetStarted = webDriver.findElement(buttonGetStartedXpath);
 
-            WebElement buttonGetStarted = webDriver.findElement(By.xpath("//form[@class='wg-header__free-trial-form wg-form bf_form_init']" +
-                    "//button[text()='Get started']"));
+        assert buttonGetStarted.isEnabled();
 
-            buttonGetStarted.click();
-            logger.info("Successful click on 'Get started for free' button...");
-        } else {
-            logger.info("There is no 'Get started for free' button...");
-            assert false;
-        }
+        logger.info("Try to click on 'Get started for free' button");
+        buttonGetStarted.click();
+        logger.info("Successful click on 'Get started for free' button...");
 
+        By inputEmailAddressXpath = By.xpath("//input[@class='wg-input modal-form-trial__input']");
+        assert isElementVisible(inputEmailAddressXpath);
 
-        logger.info("Try to input random email address...");
-        WebElement inputEmailAddress = webDriver.findElement(By.xpath("//input[@class='wg-input modal-form-trial__input']"));
-
-        assert inputEmailAddress.isDisplayed();
-        assert inputEmailAddress.isEnabled();
+        WebElement inputEmailAddress = webDriver.findElement(inputEmailAddressXpath);
 
         String randomEmailAddress = generateRandomEmailAddress();
+
+        logger.info("Try to input random email address...");
         inputEmailAddress.sendKeys(randomEmailAddress);
         logger.info("Successful input random email address...");
 
         logger.info("Try to click on 'Create Wrike account' button...");
-        WebElement buttonCreateAccount = webDriver.findElement(By.xpath("//button[@class='wg-btn wg-btn--blue modal-form-trial__submit']"));
+        By buttonCreateAccountXpath = By.xpath("//button[@class='wg-btn wg-btn--blue modal-form-trial__submit']");
+        assert isElementVisible(buttonCreateAccountXpath);
 
-        assert buttonCreateAccount.isDisplayed();
+        WebElement buttonCreateAccount = webDriver.findElement(buttonCreateAccountXpath);
         assert buttonCreateAccount.isEnabled();
 
         String currentUrl = webDriver.getCurrentUrl();
+        logger.info("Try to click on 'Create Wrike account' button...");
         buttonCreateAccount.click();
         logger.info("Successful click on 'Create Wrike account' button...");
 
@@ -77,67 +71,65 @@ public class TestWrike {
         assert !webDriver.getCurrentUrl().equals(currentUrl);
 
         // Get the whole form
-        WebElement form = webDriver.findElement(By.xpath("//form[@class='survey-form']"));
+        By surveyFormXpath = By.xpath("//form[@class='survey-form']");
+        assert isElementVisible(surveyFormXpath);
 
-        assert form.isDisplayed();
-        assert form.isEnabled();
+        WebElement surveyForm = webDriver.findElement(surveyFormXpath);
 
         // Fill form for question "How would you describe your interest in using a solution like Wrike?"
-        logger.info("Try to fill first part of form (Interest in solution)");
+        logger.info("Try to fill part of form (Interest in solution)");
 
-        WebElement interestInSolution = getRandomElementFromList(form.findElements(By.xpath("//div[@data-code='interest_in_solution']/label")));
-        assert interestInSolution.isDisplayed();
+        By interestInSolutionXpath = By.xpath("//div[@data-code='interest_in_solution']/label");
+        assert isElementVisible(interestInSolutionXpath);
+
+        WebElement interestInSolution = getRandomElementFromList(surveyForm.findElements(interestInSolutionXpath));
         assert interestInSolution.isEnabled();
 
         interestInSolution.click();
-        logger.info("Successful first part of form (Interest in solution)  ");
+        logger.info("Successful filling part of form (Interest in solution)");
 
         // Fill form for question "Ideally, how many total team members will eventually be using Wrike?"
-        logger.info("Try to fill second part of form (Team members)");
+        logger.info("Try to fill part of form (Team members)");
 
-        WebElement teamMembers = getRandomElementFromList(form.findElements(By.xpath("//div[@data-code='team_members']/label")));
-        assert teamMembers.isDisplayed();
-        assert teamMembers.isEnabled();
+        By teamMembersXpath = By.xpath("//div[@data-code='team_members']/label");
+        assert isElementVisible(teamMembersXpath);
+        WebElement teamMembers = getRandomElementFromList(surveyForm.findElements(teamMembersXpath));
 
+        teamMembers.isEnabled();
         teamMembers.click();
-        logger.info("Successful second part of from (Team members) ");
+        logger.info("Successful filling part of from (Team members) ");
 
         // Fill form for question "Does your team follow a process for managing work?"
         logger.info("Try to fill third part of form (Primary Business)");
 
-        WebElement primaryBusiness = getRandomElementFromList(form.findElements(By.xpath("div[@data-code='primary_business']/label")));
-        assert primaryBusiness.isDisplayed();
-        assert primaryBusiness.isEnabled();
+        By primaryBusinessXpath = By.xpath("//div[@data-code='primary_business']/label");
+        assert isElementVisible(primaryBusinessXpath);
+        WebElement primaryBusiness = getRandomElementFromList(surveyForm.findElements(primaryBusinessXpath));
 
+        primaryBusiness.isEnabled();
         primaryBusiness.click();
-//        If uncomment throw org.openqa.selenium.ElementNotInteractableException: element not interactable
-//        if (primaryBusiness.getText().equals("Other")) {
-//            primaryBusiness.findElement(By.xpath("//input")).sendKeys(new String[] {generateRandomString()});
-//        }
         logger.info("Successful third part of form (Primary Business) ");
 
         logger.info("Try to click on 'Resend email' button");
-//        TODO: Optimize XPath request 1
-        WebElement buttonResendEmail = webDriver.findElement(By.xpath("//div[@class='wg-grid']/div/p/button"));
-        assert buttonResendEmail.isDisplayed();
-        assert buttonResendEmail.isEnabled();
+//        By buttonResendEmailXpath = By.xpath("//div[@class='wg-grid']/div/p/button");
+        By buttonResendEmailXpath = By.xpath("//button[text()='Resend email' and contains(@class,'wg-btn--hollow button')]");
+        assert isElementVisible(buttonResendEmailXpath);
+        WebElement buttonResendEmail = webDriver.findElement(buttonResendEmailXpath);
 
+        assert buttonResendEmail.isEnabled();
         buttonResendEmail.click();
+
         logger.info("Successful click on 'Resend email' button  ");
 
-//        Check that section "Follow us" at the site footer contains the "Twitter" button that leads to the correct url and has the correct icon;
-        logger.info("Check Follow us section: Twitter");
-//        TODO: Optimize XPath request 2
-        WebElement followTwitterWrike = webDriver.findElement(By.xpath("//ul[@class='wg-footer__social-list']//a[@href='https://twitter.com/wrike']/.."));
-        assert followTwitterWrike.isDisplayed();
-        assert followTwitterWrike.isEnabled();
+        logger.info("Check Follow us section: Twitter link");
 
-        logger.info("Successful check Twitter  ");
+        By followTwitterWrikeXpath = By.xpath("//li[@class='wg-footer__social-item']//a[@href='https://twitter.com/wrike' and @rel='dofollow']/..");
+        assert isElementVisible(followTwitterWrikeXpath);
 
-//        TODO: Check logo of Twitter
+        logger.info("Successful check Twitter link");
+
         logger.info("Check correct logo for Twitter");
-        assert followTwitterWrike.findElement(By.xpath("//*[local-name()='use' and @*|href='/content/themes/wrike/dist/img/sprite/vector/footer-icons.symbol.svg?v2#twitter']")).isDisplayed();
-//        assert followTwitterWrike.findElement(By.xpath("//*[contains(@*|href,'twitter')]")).isDisplayed();
+        assert isElementVisible(By.xpath("//*[local-name()='use' and contains(@*|href,'twitter')]"));
         logger.info("Successful check correct logo for Twitter ");
 //        TODO: Report by allure plugin
     }
@@ -146,6 +138,15 @@ public class TestWrike {
     public static void quitWebDriver() {
         webDriver.quit();
         logger.info("Web driver quits");
+    }
+
+    private boolean isElementVisible(By query) {
+        if (webDriver.findElements(query).size() != 0) {
+            return true;
+        } else {
+            logger.info("No element for query:" + query.toString());
+            return false;
+        }
     }
 
     /**
